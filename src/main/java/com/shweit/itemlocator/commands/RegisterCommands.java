f46fb4a7fbf9b71bcad26bf9c950084599094db5
Dennis van den Brock <dennis.vandenbrock@easybill.de>
2024-09-10 09:15:00 +0200

    Added Command support

--- src/main/java/com/shweit/itemlocator/commands/RegisterCommands.java
+++ src/main/java/com/shweit/itemlocator/commands/RegisterCommands.java
@@ -1,16 +1,10 @@
 package com.shweit.itemlocator.commands;
 
 import com.shweit.itemlocator.ItemLocator;
-import com.shweit.itemlocator.commands.version.VersionCommand;
-
-import java.util.List;
 
 public abstract class RegisterCommands {
     public static void register() {
-        List<SubCommand> subCommands = List.of(
-            new VersionCommand()
-        );
-        CommandManager mainCommandManager = new CommandManager(subCommands);
+        CommandManager mainCommandManager = new CommandManager();
         ItemLocator.getInstance().getCommand("itemlocator").setExecutor(mainCommandManager);
     }
 }
